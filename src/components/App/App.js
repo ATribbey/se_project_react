932eb0594b1bab85eefde26c30ebd91e193dc0a3
ATribbey <alx_036@yahoo.com>
2023-12-27 06:53:40 -0500

    import header to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import logo from "../../logo.svg";
 import "./App.css";
+import Header from "../Header/Header.js";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Header />
     </>
   );
 }
