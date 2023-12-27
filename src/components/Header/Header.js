21dbd552ec16a7272c965bf7eb8c94054290d787
ATribbey <alx_036@yahoo.com>
2023-12-27 06:46:02 -0500

    add header class, images folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <div>Logo</div>
+      <div>Current Date</div>
+      <div>Current Location</div>
+      <button>Add New Clothes</button>
+      <div>User Name</div>
+      <div>User Avatar</div>
+    </>
+  );
+}
+
+export default Header;
