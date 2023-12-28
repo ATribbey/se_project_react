660ce0226d8e64defee1f6b0fefbde085ce38e72
ATribbey <alx_036@yahoo.com>
2023-12-28 13:36:00 -0500

    use semantic html in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css";
 import logo from "../../images/logo.svg";
 import avatar from "../../images/userAvatar.png";
 
-function getTime() {
+function getDate() {
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
@@ -20,14 +20,14 @@ function Header() {
     <header className="header">
       <div className="header__logo">
         <img src={logo} alt="WTWR Logo" />
-        <div>{getTime()}</div>
-        <div>Current Location</div>
+        <p className="header__date">{getDate()}</p>
+        <p className="header__location">Current Location</p>
       </div>
       <div className="header__logo">
         <button type="button" className="header__button">
           + Add clothes
         </button>
-        <div>Terrence Tegegne</div>
+        <p>Terrence Tegegne</p>
         <img src={avatar} alt="Profile Avatar" />
       </div>
     </header>
