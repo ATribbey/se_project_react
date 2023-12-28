1bbac3a2483e1ddae33342a5e335750b2849fe6a
ATribbey <alx_036@yahoo.com>
2023-12-28 16:08:44 -0500

    use semantic html in weathercard

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -3,14 +3,14 @@ import daySunny from "../../images/weathercard-banners/day-sunny.svg";
 
 function WeatherCard() {
   return (
-    <div className="weather">
+    <section className="weather">
       <p className="weather__temp">75°F</p>
       <img
         src={daySunny}
         alt="Current Weather Banner"
         className="weather__banner"
       />
-    </div>
+    </section>
   );
 }
 
