dbb5abcc484e6265d954a0815f273f3962a95379
ATribbey <alx_036@yahoo.com>
2023-12-28 12:15:19 -0500

    implement gettime functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,25 @@ import "./Header.css";
 import logo from "../../images/logo.svg";
 import avatar from "../../images/userAvatar.png";
 
+function getTime() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <>
+      <p>{currentDate}</p>
+    </>
+  );
+}
+
 function Header() {
   return (
     <header className="header">
       <div className="header__logo">
         <img src={logo} alt="WTWR Logo" />
-        <div>Current Time{getTime}</div>
+        <div>{getTime()}</div>
         <div>Current Location</div>
       </div>
       <div className="header__logo">
@@ -17,15 +30,6 @@ function Header() {
       </div>
     </header>
   );
-}
-
-function getTime() {
-  const currentDate = new Date().toLocaleString("default", {
-    month: "long",
-    day: "numeric",
-  });
-
-  return currentDate;
 }
 
 export default Header;
