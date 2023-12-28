36bc1347ba312aa0fcb574f01e66b4c46958d036
ATribbey <alx_036@yahoo.com>
2023-12-28 11:58:14 -0500

    adjust layout of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,21 @@
 import "./Header.css";
 import logo from "../../images/logo.svg";
+import avatar from "../../images/userAvatar.png";
 
 function Header() {
   return (
-    <>
-      <img src={logo} alt="WTWR Logo" />
-      <div>{getTime}</div>
-      <div>Current Location</div>
-      <button>Add New Clothes</button>
-      <div>User Name</div>
-      <div>User Avatar</div>
-    </>
+    <header className="header">
+      <div className="header__logo">
+        <img src={logo} alt="WTWR Logo" />
+        <div>Current Time{getTime}</div>
+        <div>Current Location</div>
+      </div>
+      <div className="header__logo">
+        <button type="button">+ Add clothes</button>
+        <div>Terrence Tegegne</div>
+        <img src={avatar} alt="Profile Avatar" />
+      </div>
+    </header>
   );
 }
 
