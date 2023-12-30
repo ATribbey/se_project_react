85fa1086debac4a61bbe0b4ec43f01cd10a5adfe
ATribbey <alx_036@yahoo.com>
2023-12-30 13:46:02 -0500

    add itemcard styles

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -3,16 +3,20 @@ import defaultClothingItems from "../../utils/DefaultClothing";
 
 function ItemCard() {
   return (
-    <>
+    <section className="itemcard">
       {defaultClothingItems.map((item) => {
         return (
-          <>
-            <p>{item.name}</p>
-            <img src={item.link} alt="Clothing Item" />
-          </>
+          <div className="itemcard__card">
+            <p className="itemcard__name">{item.name}</p>
+            <img
+              className="itemcard__image"
+              src={item.link}
+              alt="Clothing Item"
+            />
+          </div>
         );
       })}
-    </>
+    </section>
   );
 }
 
