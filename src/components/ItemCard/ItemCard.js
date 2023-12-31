dd3adc522f4bce30f7d0f12be0d1e6ec29ca74ca
ATribbey <alx_036@yahoo.com>
2023-12-31 15:18:05 -0500

    refactor itemcard to unordered list

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -3,10 +3,16 @@ import "./ItemCard.css";
 function ItemCard({ item }) {
   return (
     <>
-      <div className="itemcard__card">
-        <p className="itemcard__name">{item.name}</p>
-        <img className="itemcard__image" src={item.link} alt="Clothing Item" />
-      </div>
+      <ul className="itemcard__list">
+        <li className="itemcard__card">
+          <p className="itemcard__name">{item.name}</p>
+          <img
+            className="itemcard__image"
+            src={item.link}
+            alt="Clothing Item"
+          />
+        </li>
+      </ul>
     </>
   );
 }
