a7cf1ca8f37b6e5cee554f1486c95237f45a23de
ATribbey <alx_036@yahoo.com>
2024-01-11 13:19:57 -0500

    add first api request to apijs

---
+++ src/utils/api.js
@@ -0,0 +1,27 @@
+// getInitialCards() {
+//     return fetch(this._cardPath, {
+//       method: "GET",
+//       headers: this._baseHeaders,
+//     }).then((res) => {
+//       return this._checkResponse(res);
+//     });
+// //   }
+
+const baseUrl = "http://localhost:3001";
+const baseHeader = "Content-Type: application/json";
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+}
+
+export function getClothingItems() {
+  return fetch(`${baseUrl}/items`, {
+    method: "GET",
+    headers: baseHeader,
+  }).then((res) => {
+    checkResponse(res);
+  });
+}
