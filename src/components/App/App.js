ae3aabdc2bdc476881b38d1e8c6556d890d36310
ATribbey <alx_036@yahoo.com>
2024-02-23 10:44:48 -0500

    remove unnecessary commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,8 +20,6 @@ import ItemModal from "../ItemModal/ItemModal.js";
 import Profile from "../Profile/Profile.js";
 import Footer from "../Footer/Footer.js";
 
-// Mongo DB Run command:  "C:\Program Files\MongoDB\Server\7.0\bin\mongod.exe" --dbpath="c:\data\db"
-
 function App() {
   const [activeModal, setActiveModal] = useState("");
   const [selectedCard, setSelectedCard] = useState({});
