5264422d7f0c10db486f2774b4feac2eb148a4f9
ATribbey <alx_036@yahoo.com>
2024-02-23 17:55:35 -0500

    add structure for register modal

--- src/components/RegisterModal/RegisterModal.js
+++ src/components/RegisterModal/RegisterModal.js
@@ -10,7 +10,54 @@ function RegisterModal({ onClose }) {
       onClose={onClose}
       buttonText="Sign Up"
       onSubmit={onSubmit}
-    ></ModalWithForm>
+    >
+      <label htmlFor="email-input" className="modal__input-title">
+        Email*
+      </label>
+      <input
+        id="email-input"
+        className="modal__input"
+        type="email"
+        name="email"
+        placeholder="Email"
+        required
+      ></input>
+      <label htmlFor="password-input" className="modal__input-title">
+        Password*
+      </label>
+      <input
+        id="password-input"
+        className="modal__input"
+        type="password"
+        name="password"
+        placeholder="Password"
+        required
+      ></input>
+      <label htmlFor="name-input" className="modal__input-title">
+        Name*
+      </label>
+      <input
+        id="name-input"
+        className="modal__input"
+        type="text"
+        name="name"
+        placeholder="Name"
+        minLength={1}
+        maxLength={30}
+        required
+      ></input>
+      <label htmlFor="avatar-input" className="modal__input-title">
+        Avatar URL*
+      </label>
+      <input
+        id="avatar-input"
+        className="modal__input"
+        type="url"
+        name="url"
+        placeholder="Avatar URL"
+        required
+      ></input>
+    </ModalWithForm>
   );
 }
 
