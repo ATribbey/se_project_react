293c885f223d88727d4ab54eb0c101e04f869c05
ATribbey <alx_036@yahoo.com>
2024-02-23 17:48:04 -0500

    add structure for login modal, edit modal.css

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -9,7 +9,30 @@ function LoginModal({ onClose }) {
       onClose={onClose}
       buttonText="Log In"
       onSubmit={onSubmit}
-    ></ModalWithForm>
+    >
+      <label htmlFor="email-input" className="modal__input-title">
+        Email
+      </label>
+      <input
+        id="email-input"
+        className="modal__input"
+        type="email"
+        name="email"
+        placeholder="Email"
+        required
+      ></input>
+      <label htmlFor="password-input" className="modal__input-title">
+        Password
+      </label>
+      <input
+        id="password-input"
+        className="modal__input"
+        type="password"
+        name="password"
+        placeholder="Password"
+        required
+      ></input>
+    </ModalWithForm>
   );
 }
 
