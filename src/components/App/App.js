0008418ccabac9850878efd83941364bf4dc1725
ATribbey <alx_036@yahoo.com>
2024-02-28 09:11:11 -0500

    add css for updated card component containing like button and accompanying classes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -145,6 +145,32 @@ function App() {
       });
   }
 
+  // const handleCardLike = ({ id, isLiked }) => {
+  //   const token = localStorage.getItem("jwt");
+  //   // Check if this card is now liked
+  //   isLiked
+  //     ? // if so, send a request to add the user's id to the card's likes array
+  //       api
+  //         // the first argument is the card's id
+  //         .addCardLike(id, token)
+  //         .then((updatedCard) => {
+  //           setClothingItems((cards) =>
+  //             cards.map((c) => (c._id === id ? updatedCard : c))
+  //           );
+  //         })
+  //         .catch((err) => console.log(err))
+  //     : // if not, send a request to remove the user's id from the card's likes array
+  //       api
+  //         // the first argument is the card's id
+  //         .removeCardLike(id, token)
+  //         .then((updatedCard) => {
+  //           setClothingItems((cards) =>
+  //             cards.map((c) => (c._id === id ? updatedCard : c))
+  //           );
+  //         })
+  //         .catch((err) => console.log(err));
+  // };
+
   function checkTokenSetUser(token) {
     return checkToken(token).then((res) => {
       setLoggedIn(true);
--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,18 +1,33 @@
 import "./ItemCard.css";
 
-function ItemCard({ item, onSelectCard, handleOpenItemModal }) {
+function ItemCard({ item, onSelectCard, handleOpenItemModal, onCardLike }) {
+  function handleLike() {
+    onCardLike();
+  }
+
   return (
     <li className="itemcard__card">
-      <p className="itemcard__name">{item.name}</p>
-      <img
-        className="itemcard__image"
-        src={item.imageUrl}
-        alt={item.name}
-        onClick={() => {
-          onSelectCard(item);
-          handleOpenItemModal();
-        }}
-      />
+      <div className="itemcard__container">
+        <div className="itemcard__info">
+          <div className="itemcard__name-container">
+            <p className="itemcard__name">{item.name}</p>
+          </div>
+          <button
+            className="itemcard__likebutton"
+            type="button"
+            onClick={handleLike}
+          ></button>
+        </div>
+        <img
+          className="itemcard__image"
+          src={item.imageUrl}
+          alt={item.name}
+          onClick={() => {
+            onSelectCard(item);
+            handleOpenItemModal();
+          }}
+        />
+      </div>
     </li>
   );
 }
